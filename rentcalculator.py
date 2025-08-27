672f01e721bb10fe8f7dcdf6c5747e826f038e74
saadkhawar15 <saadkhawar169@gmail.com>
2025-08-27 15:37:24 +0500

    rent

---
+++ rentcalculator.py
@@ -0,0 +1,4 @@
+rent=int(input("enter rent of the house"))
+food=int(input("enter the amount of food order"))
+electricity_spend=int(input("enter the toal of electricity spend "))
+charger_per_unit=int(input("enter charge per unit in the area"))
